a296af7c104a6eeadae8816dd9d88bbb79194a1b
hp027 <hp027@foxmail.com>
2021-03-30 18:20:07 +0800

    ignore build directory

--- build/lib/numpygraph/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = __VERISON__ = "0.0.1.3"
-from .tools import CommandClient
-import fire
-def cmd():
-    fire.Fire(CommandClient)
--- build/lib/numpygraph/tools/__init__.py
+++
@@ -1,9 +0,0 @@
-from .arraydict import ArrayDict
-from .arraylist import ArrayList
-# from .arraylist_from_to import ArrayListFromTo
-# from .fileshutil import SplitFile, ThreadFileIterator
-from .npg import CommandClient
-__ALL__ = ['CommandClient']
-
-
-
