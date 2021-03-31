ce3c7a66673dbf6d3bfbcf887e96a815d59b363f
Hanry <54668181+hanryxu@users.noreply.github.com>
2021-03-31 01:09:44 -0700

    Add graph generator

--- npgraph/tools/hash.py
+++ npgraph/tools/hash.py
@@ -4,5 +4,3 @@ NODE_TYPE_MASK = 2 ** 60 - 1
 
 def chash(t, s):
     return t | (hash(s) & NODE_TYPE_MASK)
-
-# TODO: add helper function that determines the chunk location (return path to chunk file given chunk num and data form.
