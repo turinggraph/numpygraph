114122d97604d35540e946985c95111443b2cfec
hp027 <hp027@foxmail.com>
2021-04-01 17:27:15 +0800

    TST: API: add fetch and sample api stubs

---
+++ numpygraph/test/graph_api_test.py
@@ -0,0 +1,25 @@
+def fetch_node_attr(_id):
+    pass
+
+
+def fetch_edge_attr(fid, tid):
+    pass
+
+
+def fetch_node_neibor_nodes(_id):
+    pass
+
+
+def fetch_node_neibor_edges(_id):
+    pass
+
+
+def sample_node_with_degree(_id, degree=[4, 4, 4]):
+    nodes, edges = [], []
+    return nodes, edges
+
+
+def random_sample_nodes(_id, size=0):
+    pass
+
+
